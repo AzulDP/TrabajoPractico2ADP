035a079bff93472395b68f14850b83057e952982
Azul De Pol <azul.depol@gmail.com>
2023-03-19 15:46:51 +0000

    complete el ejercicio 8 que era hasta donde habia llegado con el 7 basicamente

--- js/8-ejercicio8.js
+++ js/8-ejercicio8.js
@@ -1,6 +1,7 @@
 // Dificultad:  🟢🟡
-// 8- Crea script para generar pirámide siguiente con los números del 1 al número que indique el usuario (no mayor de 50) 
-
+/* 8- Crea script para generar pirámide siguiente con los números 
+del 1 al número que indique el usuario (no mayor de 50) 
+*/
 // 1
 // 12
 // 123
@@ -8,3 +9,22 @@
 // 12345
 // 123456
 // ……
+
+let maxNum = parseInt(prompt('Ingrese un numero del 1 al 50'));
+
+if (maxNum > 50) {
+    alert('El número ingresado no es válido')
+} else {
+
+function generatePyramid() {
+    let output = '';
+    for (let i = 1; i<=maxNum; i++) {
+        for (let j =1; j<= i; j++) {
+            output += i;
+        }
+            console.log(output);
+            output = '';
+    }
+}
+generatePyramid();
+}
