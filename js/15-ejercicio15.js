e40e4e0425edbe89733b1617cb3c16acd5affe7a
Azul De Pol <azul.depol@gmail.com>
2023-03-19 14:20:59 +0000

    resolvi el ejercicio 16

--- js/15-ejercicio15.js
+++ js/15-ejercicio15.js
@@ -1,3 +1,6 @@
 // Dificultad:  🟢🟡
 // 15- Realiza un script que cuente el número de vocales que tiene un texto.
 
+const text = "The quick brown fox jumps over the lazy dog."
+const vowels = 
+
--- js/16-ejercicio16.js
+++ js/16-ejercicio16.js
@@ -1,2 +1,9 @@
 // Dificultad:  🟢🟡
 // 16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
+
+let sentence = prompt('Ingrese una cadena de texto');
+
+function reverseString(str){
+   return str.split("").reverse().join("");
+}
+document.write(reverseString(sentence));
